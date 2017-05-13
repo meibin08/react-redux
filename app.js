917862d39689f3bdded2d3860bd06d8cf58d5d59
Bin <meibin08@163.com>
2017-05-13 21:48:43 +0800

    seo

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 var express = require('express');
+var path = require('path');
 var config = require('./server/config');
 
 var app = express();
 require('./server/express')(app);
+app.use('/react-redux',express.static(path.join(__dirname, 'assets')));
 app.use(express.static(config.root + '/assets'));
-app.use('/JsonApi',express.static(path.join(__dirname, 'src')));
 require('./server/server')(app);
 require('./server/error')(app);
 require('./bin/www')(app);
